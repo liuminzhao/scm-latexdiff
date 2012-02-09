9c7b6316e3583efd73fd70f59c6821e1632dd710
Paul Hiemstra <paul.hiemstra@knmi.nl>
2012-02-09 09:56:11 +0000

    Initial commit of the code

---
+++ liblatexdiff.py
@@ -0,0 +1,22 @@
+
+
+def runCommand(cmd, stdout = None):
+    ''' Runs the command and returns an appropriate string '''
+    try:
+      retcode = subprocess.call(cmd, stdout = stdout)
+      if retcode == 0:
+          message = "OK"
+      else:
+          message = "failed (retcode: %s)" % retcode
+    except OSError, e:
+        message = "failed, %s" % e
+    return(message)
+
+def latexdiff(old_tex, new_tex, diff_tex = None):
+  ''' Run latexdiff for the above input '''
+  print "Running latexdiff: %s" % runCommand(("latexdiff", old_tex, new_tex), stdout = diff_tex)
+  print "diff stored in %s" % diff_tex.name
+
+def pdflatex(tex_file, log_file = None):
+  ''' Run pdflatex on the resulting tex file '''
+  print "Running pdflatex: %s" % runCommand(("pdflatex", tex_file), stdout = log_file)
