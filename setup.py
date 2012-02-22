edd3d7143af672408f907ff55d8f51a953940635
Ronald van Haren <ronald@archlinux.org>
2012-02-22 11:10:11 +0000

    move more stuff around

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
 	author_email="Paul Hiemstra <paul@numbertheory.nl>, Ronald van Haren <ronald@archlinux.org>",
 	url="https://bitbucket.org/paulhiemstra/scm-latexdiff",
     license="GPL v3",
-    packages=["python-scm-latexdiff"], 
+    packages=["scm_latexdiff"], 
     scripts=["bin/scm-latexdiff"],
     long_description=open('README').read(),
 )
